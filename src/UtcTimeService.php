bbb7ad326a822731c2cf0a04d92132a1c6233202
Edo Barnas <iam@edobarnas.com>
2022-02-17 19:42:19 +0100

    Define new `UTC` time service

---
+++ src/UtcTimeService.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Timekeeper;
+
+use DateTimeImmutable;
+use DateTimeZone;
+
+final class UtcTimeService implements TimeService
+{
+    public function __construct(
+        private TimeService $timeService
+    ) {
+    }
+
+    public function measure(): DateTimeImmutable
+    {
+        return $this->timeService->measure()->setTimezone(new DateTimeZone('UTC'));
+    }
+}
---
+++ tests/UtcTimeServiceTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Timekeeper\Test;
+
+use PHPUnit\Framework\TestCase;
+use Tuzex\Timekeeper\SystemTimeService;
+use Tuzex\Timekeeper\UtcTimeService;
+
+final class UtcTimeServiceTest extends TestCase
+{
+    public function testItReturnsDateTimeWithUtcTimeZone(): void
+    {
+        $timeService = new UtcTimeService(
+            new SystemTimeService()
+        );
+
+        $dateTime = $timeService->measure();
+
+        $this->assertSame(0, $dateTime->getOffset());
+    }
+}
